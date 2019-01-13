18393879ed68059638620fdaa9a252ebc878fe38
Charles Burgess <consulting@cvburgess.com>
2019-01-13 16:27:35 -0500

    add Urql example

---
+++ src/Urql.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Provider, Client, Connect, query } from "urql";
+
+const client = new Client({
+  url: "http://localhost:4000/graphql"
+});
+
+const ALL_BOOKS_QUERY = `
+{
+  books {
+    title
+    author {
+      name
+    }
+  }
+}`;
+
+const Library = () => (
+  <Provider client={client}>
+    <Connect query={query(ALL_BOOKS_QUERY)}>
+      {({ loaded, data }) => (
+        <div>
+          <h1>{`The library is ${loaded ? "open" : "loading..."}`}</h1>
+          {data &&
+            data.books.map(book => (
+              <p>
+                <i>{book.title}</i> by {book.author.name}
+              </p>
+            ))}
+        </div>
+      )}
+    </Connect>
+  </Provider>
+);
+
+export default Library;
